ac1363546fcdaced5810d791b7a01df94af07e03
Rattlehead90 <strogonov@seznam.cz>
2022-07-04 15:19:48 +0200

    Stretch the drawing field across the drawing-box

--- script.js
+++ script.js
@@ -6,8 +6,8 @@ function drawDrawingGrid(rows, cols) {
     drawingBox.style.setProperty('--grid-cols', cols);
     for (i = 0; i < rows*cols; i++) {
         let drawingPixel = document.createElement('div');
-        drawingBox.appendChild(drawingPixel).className = 'drawing-pixel';
         drawingPixel.innerText = i + 1;
+        drawingBox.appendChild(drawingPixel).className = 'drawing-pixel';
     }
 }
 
