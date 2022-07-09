f1b59864582ea9dac8278a3f47fff41e40661e23
Rattlehead90 <strogonov@seznam.cz>
2022-07-09 20:57:09 +0200

    Add 'Rainbow mode' for random coloring
    Basically it's an alpha version of the project. It might be improved by
    adding a better color picker and size slider.

--- script.js
+++ script.js
@@ -10,7 +10,6 @@ function drawDrawingGrid(size) {
     }
 }
 
-
 drawDrawingGrid(16);
 
 function changeSize(size) {
@@ -45,12 +44,28 @@ window.onclick = function(event) {
 //Painting logic
 
 function paint(color) {
+  console.log(color)
   const drawingPixels = document.querySelectorAll('.drawing-pixel');
-  for (let i = 0; i < drawingPixels.length; i++) {
+  if (color === 'rainbow') {
+
+    for (let i = 0; i < drawingPixels.length; i++) {
+      let rgb1 = Math.floor(Math.random()*256);
+      let rgb2 = Math.floor(Math.random()*256);
+      let rgb3 = Math.floor(Math.random()*256);
+      drawingPixels[i].addEventListener('mouseover', () => {
+          console.log(`rgb(${rgb1}, ${rgb2}, ${rgb3})`);
+          drawingPixels[i].style.backgroundColor = `rgb(${rgb1}, ${rgb2}, ${rgb3})`;
+        })
+      }
+  } else {
+    for (let i = 0; i < drawingPixels.length; i++) {
       drawingPixels[i].addEventListener('mouseover', () => {
           drawingPixels[i].style.backgroundColor = color;
         })
+    }
   }
+
+
 }
 
 paint('black');
@@ -63,6 +78,8 @@ const redOption = document.getElementById('red');
 redOption.onclick = () => paint('red');
 const blackOption = document.getElementById('black');
 blackOption.onclick = () => paint('black');
+const rainbowOption = document.getElementById('rainbow');
+rainbowOption.onclick = () => paint('rainbow');
 
 //Reset logic
 
